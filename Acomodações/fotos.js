cd99f9d232c23afa839b2934fd00110a70ad7735
santanagabi <gaboliveirasantana1@gmail.com>
2022-12-11 16:37:08 -0300

    Subindo novas páginas

---
+++ Acomodações/fotos.js
@@ -0,0 +1,36 @@
+function img01 (){
+    document.getElementById("trocarimg").src="img/trocaimg01.jpg";
+}
+function img02 (){
+    document.getElementById("trocarimg").src="img/trocaimg02.jpg";
+}
+function img03 (){
+    document.getElementById("trocarimg").src="img/trocaimg03.jpg";
+}
+function img04 (){
+    document.getElementById("trocarimg").src="img/trocaimg04.jpg";
+}
+function img05 (){
+    document.getElementById("trocarimg").src="img/trocaimg05.jpg";
+}
+function img06 (){
+    document.getElementById("trocarimg").src="img/trocaimg06.jpg";
+}
+function img07 (){
+    document.getElementById("trocarimg").src="img/trocaimg07.jpg";
+}
+function img08 (){
+    document.getElementById("trocarimg").src="img/trocaimg08.jpg";
+}
+function img09 (){
+    document.getElementById("trocarimg").src="img/trocaimg09.jpg";
+}
+function img10 (){
+    document.getElementById("trocarimg").src="img/trocaimg10.jpg";
+}
+function img11 (){
+    document.getElementById("trocarimg").src="img/trocaimg11.jpg";
+}
+function img12 (){
+    document.getElementById("trocarimg").src="img/trocaimg12.jpg";
+}
